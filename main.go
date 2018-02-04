19ccfd2e48962b574741b99e758e81a8b92f6c85
dalongrong <1141591465@qq.com>
2018-02-04 23:28:20 +0800

    init project

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"com.dalongapp/golearning/users"
+)
+
+func main() {
+	var user = users.User{Name: "dalofffffffffng", Age: 44}
+	fmt.Printf("dalong" + "世界")
+	fmt.Printf(user.Name)
+}
