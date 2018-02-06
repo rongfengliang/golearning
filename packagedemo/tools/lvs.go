05438b1a367d6b113cf3dd8c95b6572fc7dc4e59
dalongrong <1141591465@qq.com>
2018-02-06 22:20:22 +0800

    add commont for func

--- packagedemo/tools/lvs.go
+++ packagedemo/tools/lvs.go
@@ -6,14 +6,17 @@ import (
 	"github.com/rongfengliang/golearning/packagedemo/log"
 )
 
+// print lvs init information
 func LVSINIT() {
 	fmt.Println("lvs init")
 }
 
+// print lvs detail information
 func LVSDetailInfo() {
 	log.Info("dalongdemo.....lvs")
 }
 
+// print lvs error infoarmation
 func LVSAPPErrorInfo() {
 	log.Error("dalongdemo....lvs debug ingo")
 }
